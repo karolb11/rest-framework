a76fcb3ba104ed96de2e75e3d7d35ee254df6297
bronieck <karol.broniec@contractors.roche.com>
2023-11-15 17:12:04 +0100

    improve ComposedListValidator

---
+++ framework/src/main/java/com/broniec/rest/famework/validator/ComposedListConstraint.java
@@ -0,0 +1,39 @@
+package com.broniec.rest.famework.validator;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+class ComposedListConstraint<T> implements Constraint<List<T>> {
+
+    private final Validator<T> validator;
+
+    ComposedListConstraint(String fieldLabel, Validator<T> validator) {
+        this.validator = validator;
+    }
+
+
+    @Override
+    public Stream<ConstraintViolation> check(List<T> value, String fieldLabel) {
+        var resultBuilder = Stream.<ConstraintViolation>builder();
+
+        if (nonNull(value)) {
+            IntStream.range(0, value.size()).forEach(i -> {
+                validator.validate(value.get(i)).stream().map(violation -> {
+                    String field;
+                    if (isNull(violation.field())) {
+                        field = fieldLabel + "[%s]".formatted(i);
+                    } else {
+                        field = fieldLabel + "[%s]".formatted(i) + "." + violation.field();
+                    }
+                    return new ConstraintViolation(field, violation.message());
+                }).forEach(resultBuilder::add);
+            });
+        }
+
+        return resultBuilder.build();
+    }
+}
--- framework/src/main/java/com/broniec/rest/famework/validator/ComposedListValidator.java
+++ framework/src/main/java/com/broniec/rest/famework/validator/ComposedListValidator.java
@@ -4,50 +4,28 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 
 public class ComposedListValidator<T, K> implements ValidationRules<T> {
 
     private final Function<T, List<K>> valueGetter;
     private final String fieldLabel;
     private final Validator<K> validator;
-    private final Collection<Constraint<List<?>>> constraints;
+    private final Collection<Constraint<List<K>>> constraints;
 
     ComposedListValidator(Function<T, List<K>> valueGetter, String fieldLabel, ValidationConfig<K> validationConfig) {
         this.valueGetter = valueGetter;
         this.fieldLabel = fieldLabel;
         this.validator = new Validator<>(validationConfig);
         this.constraints = new ArrayList<>();
+        constraints.add(new ComposedListConstraint<K>(fieldLabel, validator));
     }
 
 
     @Override
     public Stream<ConstraintViolation> execute(T obj) {
-        var resultBuilder = Stream.<ConstraintViolation>builder();
         var value = valueGetter.apply(obj);
-
-        constraints.stream().flatMap(constraint -> constraint.check(value, fieldLabel))
-                .forEach(resultBuilder::add);
-
-        if (nonNull(value)) {
-            IntStream.range(0, value.size()).forEach(i -> {
-                validator.validate(value.get(i)).stream().map(violation -> {
-                    String field;
-                    if (isNull(violation.field())) {
-                        field = fieldLabel + "[%s]".formatted(i);
-                    } else {
-                        field = fieldLabel + "[%s]".formatted(i) + "." + violation.field();
-                    }
-                    return new ConstraintViolation(field, violation.message());
-                }).forEach(resultBuilder::add);
-            });
-        }
-
-        return resultBuilder.build();
+        return constraints.stream().flatMap(constraint -> constraint.check(value, fieldLabel));
     }
 
     public ComposedListValidator<T, K> mandatory() {
