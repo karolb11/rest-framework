5205ac4f8ec589b0e564561b6e2113f9408e719b
bronieck <karol.broniec@contractors.roche.com>
2023-11-15 16:59:03 +0100

    improve ComposedListValidator

--- framework/src/main/java/com/broniec/rest/famework/validator/ComposedListValidator.java
+++ framework/src/main/java/com/broniec/rest/famework/validator/ComposedListValidator.java
@@ -1,26 +1,27 @@
 package com.broniec.rest.famework.validator;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class ComposedListValidator<T, K> implements ValidationRules<T> {
-
-    private final ConstraintViolationBuilder constraintViolationBuilder;
 
     private final Function<T, List<K>> valueGetter;
     private final String fieldLabel;
     private final Validator<K> validator;
-    private boolean mandatory;
+    private final Collection<Constraint<List<?>>> constraints;
 
     ComposedListValidator(Function<T, List<K>> valueGetter, String fieldLabel, ValidationConfig<K> validationConfig) {
-        this.constraintViolationBuilder = new ConstraintViolationBuilder(fieldLabel);
         this.valueGetter = valueGetter;
         this.fieldLabel = fieldLabel;
         this.validator = new Validator<>(validationConfig);
+        this.constraints = new ArrayList<>();
     }
 
 
@@ -29,30 +30,28 @@ public class ComposedListValidator<T, K> implements ValidationRules<T> {
         var resultBuilder = Stream.<ConstraintViolation>builder();
         var value = valueGetter.apply(obj);
 
-        if (isNull(value)) {
-            if (mandatory) {
-                resultBuilder.add(constraintViolationBuilder.mandatoryField());
-            }
-            return resultBuilder.build();
-        }
+        constraints.stream().flatMap(constraint -> constraint.check(value, fieldLabel))
+                .forEach(resultBuilder::add);
 
-        IntStream.range(0, value.size()).forEach(i -> {
-            validator.validate(value.get(i)).stream().map(violation -> {
-                String field;
-                if (isNull(violation.field())) {
-                    field = fieldLabel + "[%s]".formatted(i);
-                } else {
-                    field = fieldLabel + "[%s]".formatted(i) + "." + violation.field();
-                }
-                return new ConstraintViolation(field, violation.message());
-            }).forEach(resultBuilder::add);
-        });
+        if (nonNull(value)) {
+            IntStream.range(0, value.size()).forEach(i -> {
+                validator.validate(value.get(i)).stream().map(violation -> {
+                    String field;
+                    if (isNull(violation.field())) {
+                        field = fieldLabel + "[%s]".formatted(i);
+                    } else {
+                        field = fieldLabel + "[%s]".formatted(i) + "." + violation.field();
+                    }
+                    return new ConstraintViolation(field, violation.message());
+                }).forEach(resultBuilder::add);
+            });
+        }
 
         return resultBuilder.build();
     }
 
     public ComposedListValidator<T, K> mandatory() {
-        this.mandatory = true;
+        constraints.add(new MandatoryObjectConstraint<>(fieldLabel));
         return this;
     }
 
